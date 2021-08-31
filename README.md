24c7686fddc44dc86cd509c1fdb270c763c9ed7d
Bruno Gritti <glbrunogl@gmail.com>
2021-08-31 15:40:30 -0300

    package

---
+++ README.md
@@ -0,0 +1,1 @@
+Generate your vue components via terminal
---
+++ src/VueGeneratorServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Laravel\Ui;
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\ServiceProvider;
+
+class VueGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                VueMakeCommand::class
+            ]);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Route::mixin(new AuthRouteMethods);
+    }
+}
