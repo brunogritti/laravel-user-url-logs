e54d35e15dc4caf95766188d400046d310d0f12c
Bruno Gritti <glbrunogl@gmail.com>
2022-12-26 16:18:06 -0300

    getdirty

--- src/Traits/Loggable.php
+++ src/Traits/Loggable.php
@@ -11,24 +11,23 @@ trait Loggable
     {
         static::updated(function (Model $model) {
             //Get which columns changed
-            $changes = array_diff($model->getOriginal(), $model->getAttributes());
-
-            dd($model->getDirty());
+            $changes = array_keys($model->getDirty());
             
             //Creates a log for every column changed
             foreach ($changes as $key => $change) {
-                dd($change, $model[$change]);
-                $log = new UserActivityLog;
-
-                $log->user_id = \Auth::id();
-                $log->action = 'updated';
-                $log->model = get_class($model);
-                $log->column = $change;
-                $log->row = $model->id;
-                $log->data = $model[$change];
-                $log->created_at = now();
-
-                $log->save();
+                if ($model[$change] != null) {
+                    $log = new UserActivityLog;
+    
+                    $log->user_id = \Auth::id();
+                    $log->action = 'updated';
+                    $log->model = get_class($model);
+                    $log->column = $change;
+                    $log->row = $model->id;
+                    $log->data = $model[$change];
+                    $log->created_at = now();
+    
+                    $log->save();
+                }
                 //self::createLog($model, 'updated', $change, $model[$change]);
             }
         });
